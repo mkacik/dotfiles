29133d79426738f9def0ec50b9be3a31ff6bb017
Maria Kacik <m.m.kacik@gmail.com>
2013-11-26 00:56:10 +0100

    Fixed prompt lag on linux

--- bash/.bashrc.d/prompt.sh
+++ bash/.bashrc.d/prompt.sh
@@ -1,11 +1,11 @@
 function get_ruby_gemset {
- local gemset=$(rvm gemset list | grep -e '^=' | grep -v '(' | awk '{print $2}')
- local ruby=$(ruby -v | cut -f2 -d' ' | cut -f1 -d'p' )
- if [ -z "$gemset" ]; then
-  echo "$ruby"
- else
-  echo "$gemset@$ruby"
- fi
+	local gemset=$(rvm current | grep '@' | cut -d'@' -f2)
+	local ruby=$(ruby -v | cut -f2 -d' ' | cut -f1 -d'p' )
+ 	if [ -z "$gemset" ]; then
+  	echo "$ruby"
+ 	else
+  	echo "$gemset@$ruby"
+ 	fi
 }
 
 function get_git_branch {
@@ -18,7 +18,7 @@ function get_git_branch {
 }
 
 # Set prompt colors
-PS1="\[\033[01;32m\]\u@fry\[\033[00m\]:"
+PS1="\[\033[01;32m\]\u@\h\[\033[00m\]:"
 PS1="$PS1\[\033[01;35m\]\$(get_git_branch)\[\033[00m\]:"
 PS1="$PS1\[\033[01;36m\]\$(get_ruby_gemset)\[\033[00m\]:"
 PS1="$PS1\[\033[01;34m\]\w\[\033[00m\]\$ "
